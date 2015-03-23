1bbf21cc017c31b9619bb9de989717ba3d86c3aa
Hola! Yo soy Llama & Durkheim <pair+BackStreetSoy.Durkheim@devbootcamp.com>
2015-03-23 14:14:43 -0500

    proper seeding

--- adoption-agency-for-animals/app/models/animal.rb
+++ adoption-agency-for-animals/app/models/animal.rb
@@ -1,6 +1,6 @@
 class Animal < ActiveRecord::Base
-  belongs_to :owner
-  belongs_to :adopter
+  belongs_to :owner, class_name: "User"
+  belongs_to :adopter, class_name: "User"
   belongs_to :breed
   belongs_to :state
   has_one :post
