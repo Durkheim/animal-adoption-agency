8b0c849679fcd2d219a87f66f185fba0abd5743d
Hola! Yo soy Llama & Durkheim <pair+BackStreetSoy.Durkheim@devbootcamp.com>
2015-03-23 18:52:18 -0500

    working through state navigation to correct breed and animals

--- adoption-agency-for-animals/app/models/animal.rb
+++ adoption-agency-for-animals/app/models/animal.rb
@@ -2,6 +2,8 @@ class Animal < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
   belongs_to :adopter, class_name: "User"
   belongs_to :breed
+  belongs_to :species
+  # has_many :species, through: :breed
   belongs_to :state
   has_one :post
 
--- adoption-agency-for-animals/app/models/species.rb
+++ adoption-agency-for-animals/app/models/species.rb
@@ -1,4 +1,5 @@
 class Species < ActiveRecord::Base
   has_many :breeds
+  has_many :animals, through: :breeds
   has_many :posts, through: :breeds
 end
