45d3086ed999e9be909543a433c5c826ecb9a516
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2015-03-25 19:42:55 -0500

    updating the breed view

--- adoption-agency-for-animals/app/controllers/index.rb
+++ adoption-agency-for-animals/app/controllers/index.rb
@@ -1,14 +1,14 @@
-get '/' do
+get '/' do # "/login"
   erb :index
 end
 
-post '/' do
+post '/' do # "/login"
   if User.authenticate(params[:email], params[:password]) == nil
     redirect "/"
   else
     @user = User.authenticate(params[:email], params[:password])
     session[:user_id] = @user.id
-    erb :choice
+    erb :choice # redirect '/'
   end
 end
 
@@ -36,12 +36,13 @@ get '/state/:state_id' do
 end
 
 get '/state/:state_id/species/:species_id/breeds' do
-  @user = User.find_by(id: session[:user_id])
-  @species_id = params[:species_id]
-  @state_id = params[:state_id]
+  @user = User.find(session[:user_id])
+  @species = Species.find(params[:species_id])
+  @state = State.find(params[:state_id])
   @breeds = Breed.where(species_id: params[:species_id])
   erb :"breeds/show"
 end
+
 
 get '/animals/:id' do
   @user = User.find_by(id: session[:user_id])
--- adoption-agency-for-animals/app/models/breed.rb
+++ adoption-agency-for-animals/app/models/breed.rb
@@ -2,4 +2,5 @@ class Breed < ActiveRecord::Base
   belongs_to :species
   has_many :animals
   has_many :posts, through: :animals
+  has_many :locations, through: :animals, source: :state
 end
